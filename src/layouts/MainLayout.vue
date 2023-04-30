58ccefbab474923cd9f429688454920c5335b810
Manikant <kellamanikant@gmail.com>
2023-04-30 15:21:12 +0530

    initial  commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,159 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header v-if="!loading">
+      <q-toolbar>
+        <q-btn
+          @click="reload()"
+          flat
+          round
+          size="19px"
+          icon="fa-sharp fa-light fa-shuffle"
+        >
+          <q-tooltip class="bg-dark"> Random select existing</q-tooltip></q-btn
+        >
+        <q-icon flat round size="32px" name="fa-sharp fa-light fa-waveform" />
+        <q-btn
+          @click="generateNewGroove()"
+          flat
+          round
+          size="19px"
+          icon="fa-sharp fa-light fa-sparkles"
+          :disable="loadingGroove"
+          :loading="loadingGroove"
+        >
+          <q-tooltip class="bg-dark"> Generate new groove </q-tooltip></q-btn
+        >
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view :loading="loading" :groove="groove" />
+    </q-page-container>
+
+    <q-dialog v-model="loading" persistent full-width full-height>
+      <div class="flex flex-center column items-center justify-center">
+        <q-icon name="fa-sharp fa-solid fa-spinner fa-spin" size="32px" />
+        <div class="text-h6 q-mt-md">Loading groove...</div>
+      </div>
+    </q-dialog>
+  </q-layout>
+</template>
+
+<script>
+import axios from "axios";
+import supabase from "../supabase";
+import { Notify } from "quasar";
+
+export default {
+  name: "MainLayout",
+  data() {
+    return {
+      loading: true,
+      groove: null,
+      loadingGroove: false,
+    };
+  },
+  async mounted() {
+    await this.getRandomGroove();
+  },
+  methods: {
+    reload() {
+      window.location.reload();
+    },
+    handleUpdate(newValue) {
+      this.loading = newValue;
+    },
+    async getRandomGroove() {
+      const { data, error } = await supabase.rpc("get_random_groove");
+
+      if (error) {
+        console.error("Error fetching random groove:", error);
+      } else {
+        this.groove = data[0];
+        setTimeout(() => {
+          this.loading = false;
+        }, 600);
+      }
+    },
+    generateNewGroove() {
+      this.loadingGroove = true;
+
+      const dismiss = Notify.create({
+        message: "Generating new groove...",
+        position: "top",
+        spinner: true,
+        timeout: 10000,
+      });
+
+      let config = {
+        method: "post",
+        url: "supabase edge function url", 
+        headers: {
+          "Content-Type": "application/json",
+          Authorization:
+            "Bearer " +
+            "supabase auth key",
+        },
+      };
+
+      axios
+        .request(config)
+        .then((response) => {
+          if (response.data) {
+            dismiss();
+            Notify.create({
+              message: "New groove loaded",
+              position: "top",
+              icon: "fa fa-check-circle",
+              type: "positive",
+            });
+            this.groove = response.data;
+            this.loadingGroove = false;
+            this.saveGroove(this.groove);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          this.loadingGroove = false;
+        });
+    },
+    async saveGroove(groove) {
+      const { data, error } = await supabase
+        .from("grooves")
+        .insert([groove])
+        .select();
+
+      if (error) {
+        console.error("Error saving groove:", error);
+      } else {
+        console.log("Groove saved successfully:", data);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.q-header {
+  background: $dark;
+  padding: $gutter * 4 $gutter * 4 $gutter * 2 $gutter * 4;
+  .q-toolbar {
+    justify-content: space-between;
+    padding: 0px;
+  }
+}
+.fa-waveform {
+  background: linear-gradient(302.62deg, #02e3f5 -0.44%, #ee08cd 99.56%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  text-fill-color: transparent;
+}
+.q-notification.bg-positive {
+  background: linear-gradient(
+    302.62deg,
+    #02e3f5 -0.44%,
+    #ee08cd 99.56%
+  ) !important;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,299 @@
+<template>
+  <q-page class="player" v-if="!loading && groove">
+    <div class="title">
+      <h1>{{ groove.title }}</h1>
+      <p>
+        {{ groove.description }}
+      </p>
+    </div>
+
+    <div class="beats">
+      <div class="beat beat-high">
+        <button
+          v-for="(high, index) in groove.beat_high"
+          :key="'high-' + index"
+          :class="{ active: high.active, current: index === currentBeat }"
+          @click="toggleActive(high)"
+        ></button>
+      </div>
+      <div class="beat beat-mid">
+        <button
+          v-for="(mid, index) in groove.beat_mid"
+          :key="'mid-' + index"
+          :class="{ active: mid.active, current: index === currentBeat }"
+          @click="toggleActive(mid)"
+        ></button>
+      </div>
+      <div class="beat beat-low">
+        <button
+          v-for="(low, index) in groove.beat_low"
+          :key="'low-' + index"
+          :class="{ active: low.active, current: index === currentBeat }"
+          @click="toggleActive(low)"
+        ></button>
+      </div>
+    </div>
+
+    <div class="bpm">
+      <q-btn
+        icon="fa-solid fa-minus"
+        round
+        flat
+        :disable="bpm <= 20"
+        @click="down(bpm)"
+      />
+      <q-input
+        v-model.number="bpm"
+        @change="updateBPM"
+        min="20"
+        max="300"
+        step="2"
+        type="number"
+        borderless
+      />
+      <q-btn
+        icon="fa-solid fa-plus"
+        round
+        flat
+        :disable="bpm >= 300"
+        @click="up(bpm)"
+      />
+    </div>
+    <div class="action">
+      <div class="box box-1">&nbsp;</div>
+      <div class="box box-2">
+        <q-btn @click="playSequence" :disabled="!hasActiveBeat" round size="lg">
+          <q-icon
+            size="1.5em"
+            v-if="playbackState === 'playing'"
+            name="fa-sharp fa-solid
+        fa-pause"
+          />
+          <q-icon
+            v-else
+            size="1.5em"
+            name="fa-sharp fa-solid
+        fa-play"
+          />
+        </q-btn>
+      </div>
+      <div class="box box-3">
+        <q-btn
+          @click="downloadMIDI"
+          rounded
+          class="q-px-md"
+          color="white"
+          outline
+          ><q-icon
+            size="1.2em"
+            name="fa-sharp fa-light fa-arrow-down"
+            class="q-mr-sm"
+          />
+          <span>MIDI</span></q-btn
+        >
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import * as Tone from "tone";
+import MidiWriter from "midi-writer-js";
+
+export default {
+  name: "IndexPage",
+  props: {
+    groove: {
+      type: Object,
+      default: () => ({}),
+    },
+    loading: {
+      type: Boolean,
+    },
+  },
+  data() {
+    return {
+      bpm: 100,
+      currentBeat: -1,
+      playbackState: "stopped",
+    };
+  },
+  mounted() {
+    if (this.groove) {
+      this.initToneSequence(this.groove);
+    }
+  },
+  computed: {
+    hasActiveBeat() {
+      return (
+        this.groove.beat_high.some((beat) => beat.active) ||
+        this.groove.beat_mid.some((beat) => beat.active) ||
+        this.groove.beat_low.some((beat) => beat.active)
+      );
+    },
+  },
+  methods: {
+    down(bpm) {
+      this.bpm = bpm - 10;
+      Tone.Transport.bpm.value = this.bpm / 2;
+    },
+    up(bpm) {
+      this.bpm = bpm + 10;
+      Tone.Transport.bpm.value = this.bpm / 2;
+    },
+    toggleActive(beat) {
+      beat.active = !beat.active;
+    },
+    updateCurrentBeat(beatIndex) {
+      this.currentBeat = beatIndex;
+    },
+    initToneSequence(groove) {
+      const highSynth = new Tone.Sampler({
+        urls: {
+          "A#1":
+            "https://supabase.co" +
+            "/storage/v1/object/public/audio/" +
+            groove.genre +
+            "/high.wav",
+        },
+      }).toDestination();
+      const medSynth = new Tone.Sampler({
+        urls: {
+          D2:
+            "https://supabase.co" +
+            "/storage/v1/object/public/audio/" +
+            groove.genre +
+            "/mid.wav",
+        },
+      }).toDestination();
+      const lowSynth = new Tone.Sampler({
+        urls: {
+          B1:
+            "https://supabase.co" +
+            "/storage/v1/object/public/audio/" +
+            groove.genre +
+            "/low.wav",
+        },
+      }).toDestination();
+
+      const sequence = new Tone.Sequence(
+        (time, beatIndex) => {
+          Tone.Draw.schedule(() => {
+            this.updateCurrentBeat(beatIndex);
+          }, time);
+          if (groove.beat_high[beatIndex].active) {
+            highSynth.triggerAttackRelease("A#1", "8n", time);
+          }
+          if (groove.beat_mid[beatIndex].active) {
+            medSynth.triggerAttackRelease("D2", "8n", time);
+          }
+          if (groove.beat_low[beatIndex].active) {
+            lowSynth.triggerAttackRelease("B1", "8n", time);
+          }
+        },
+        Array.from({ length: groove.beat_high.length }, (_, i) => i),
+        "8n"
+      );
+
+      Tone.Transport.bpm.value = this.bpm / 2;
+
+      sequence.start();
+    },
+    playSequence() {
+      if (Tone.context.state !== "running") {
+        Tone.start();
+      }
+      if (this.playbackState === "stopped" || this.playbackState === "paused") {
+        Tone.Transport.start();
+        this.playbackState = "playing";
+      } else if (this.playbackState === "playing") {
+        Tone.Transport.pause();
+        this.playbackState = "paused";
+      }
+    },
+    updateBPM() {
+      Tone.Transport.bpm.value = this.bpm / 2;
+    },
+    clearTone() {
+      Tone.Transport.stop();
+      Tone.Transport.cancel();
+    },
+    downloadMIDI() {
+      const track = new MidiWriter.Track();
+
+      const drumKitProgramChange = new MidiWriter.ProgramChangeEvent({
+        channel: 9,
+        instrument: 0,
+      });
+      track.addEvent(drumKitProgramChange);
+
+      track.setTempo(this.groove.bpm);
+
+      const highNote = 42;
+      const midNote = 38;
+      const lowNote = 36;
+
+      this.groove.beat_high.forEach((beat, beatIndex) => {
+        if (beat.active) {
+          track.addEvent(
+            new MidiWriter.NoteEvent({
+              pitch: [highNote],
+              duration: "8",
+              startTick: beatIndex * 128,
+              channel: 9,
+            })
+          );
+        }
+
+        if (this.groove.beat_mid[beatIndex].active) {
+          track.addEvent(
+            new MidiWriter.NoteEvent({
+              pitch: [midNote],
+              duration: "8",
+              startTick: beatIndex * 128,
+              channel: 9,
+            })
+          );
+        }
+
+        if (this.groove.beat_low[beatIndex].active) {
+          track.addEvent(
+            new MidiWriter.NoteEvent({
+              pitch: [lowNote],
+              duration: "8",
+              startTick: beatIndex * 128,
+              channel: 9,
+            })
+          );
+        }
+      });
+
+      const write = new MidiWriter.Writer([track]);
+
+      const midiDataURL = write.dataUri();
+
+      const link = document.createElement("a");
+      link.href = midiDataURL;
+      link.download = this.groove.title + " - Groove AI.mid";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    },
+  },
+  watch: {
+    groove: {
+      handler(newGroove, oldGroove) {
+        if (oldGroove !== null) {
+          this.clearTone();
+          this.playbackState = "stopped";
+        }
+        if (newGroove) {
+          this.bpm = newGroove.bpm;
+          this.initToneSequence(newGroove);
+        }
+      },
+      immediate: true,
+    },
+  },
+};
+</script>
